1622caa6ce02c5a218a42deeb7d250ace7999548
Gunnar Scherf <mail@gunnar-scherf.de>
2022-12-27 17:15:24 +0100

    deployment

--- apps/client/context_processors.py
+++ apps/client/context_processors.py
@@ -2,6 +2,8 @@ import logging
 
 from django.conf import settings as site_settings
 
+from client import __version__
+
 log = logging.getLogger(__name__)
 
 
@@ -9,4 +11,5 @@ def settings(request):
     return {'brand': site_settings.BRAND,
             'sso_base_url': site_settings.OPENID_SSO_SERVER_BASE_URL,
             'app_name': site_settings.SSO['APP_NAME'],
+            'version': __version__
             }
