435bc31d1a1400e291ee4e6b22be0c0132bb6554
Gunnar Scherf <mail@gunnar-scherf.de>
2023-01-22 14:49:06 +0100

    cleanup

--- apps/docker-entrypoint.sh
+++ apps/docker-entrypoint.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env bash
-set -exf
+set -ef
 
 
-if [[ x$DJANGO_MIGRATE = xon ]]; then
+if [ "$DJANGO_MIGRATE" = "on" ]; then
 	./manage.py migrate --noinput
 fi
 
-if  [[ x$DJANGO_CREATE_SUPERUSER = xon ]]; then
+if  [ "$DJANGO_CREATE_SUPERUSER" = "on" ] || [ "$DJANGO_LOAD_INITIAL_DATA" = "on"  ]; then
   user_count=$(./manage.py user_count)
 
-  if [[ $user_count = 0  ]]; then
+  if [[ $user_count -eq 0 ]]; then
 
-    if  [[ x$DJANGO_CREATE_SUPERUSER = xon ]]; then
+    if  [ "$DJANGO_CREATE_SUPERUSER" = "on" ]; then
       ./manage.py createsuperuser --noinput
+    fi
+
+    if [ "$DJANGO_LOAD_INITIAL_DATA" = "on" ]; then
+      ./manage.py loaddata initial_data
     fi
 
   fi
