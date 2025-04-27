f38e7293b80c2f9510a55eefd0ee53db268e0dbc
Gunnar Scherf <mail@gunnar-scherf.de>
2025-04-27 11:12:31 +0200

    bootstrap 5.3.5

--- set-version.sh
+++ set-version.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-VERSION=2.2.10
+VERSION=2.2.11
 
 sed -i "s/__version__ =.*/__version__ = '${VERSION}'/" apps/client/__init__.py
+sed -i "s/^  tag:.*/  tag: ${VERSION}/" ../dwbn-demos/helmfile/oidc/values.yaml
 
