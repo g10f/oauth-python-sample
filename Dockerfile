deeae90a5d299cdc4bbeab7a361f881426116656
Gunnar Scherf <mail@gunnar-scherf.de>
2025-08-22 11:14:11 +0200

    python:3.13.7

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.13.5-slim
+FROM python:3.13.7-slim
 
 # Install packages needed to run your application (not build deps):
 ENV RUN_DEPS="postgresql-client"
--- set-version.sh
+++ set-version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=2.3.1
+VERSION=2.3.2
 
 sed -i "s/__version__ =.*/__version__ = '${VERSION}'/" apps/client/__init__.py
 sed -i "s/^  tag:.*/  tag: ${VERSION}/" ../dwbn-demos/helmfile/oidc/values.yaml
