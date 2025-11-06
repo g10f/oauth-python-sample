8866542f7b62c8e21d9304a71d59cd106e591cb0
Gunnar Scherf <mail@gunnar-scherf.de>
2025-11-06 12:55:47 +0100

    django 5.2.8

--- set-version.sh
+++ set-version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=2.4.0
+VERSION=2.4.1
 
 sed -i "s/__version__ =.*/__version__ = '${VERSION}'/" apps/client/__init__.py
 sed -i "s/^  tag:.*/  tag: ${VERSION}/" ../dwbn-demos/helmfile/oidc/values.yaml
